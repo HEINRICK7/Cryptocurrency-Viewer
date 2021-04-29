25d73ac0ac74da5b75879f83ba552fd5bc600adc
Henrique <Carlos Henrique>
2021-04-29 08:43:22 -0300

    modified files

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 import { Statistic, Row, Col } from 'antd';
 import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
 
@@ -10,37 +9,34 @@ const Coin = ({ id ,name, image, symbol, price, volume, high_24h, chart }) => {
     return (
         <>
         <div className="coin_container">
-            <Link to={`./coins/${ id }`}>
-                <div className="coin">
-                    <img src={image} className="coin_image" alt="crypto" />
-                    <h1 className="coin_name">{name}</h1>
-                    <p className="coin_symbol">{symbol}</p>
-                    <Row>
-                    <p className="price_high_24">24hrs</p>
-                        <Col className="high_42">
-                            <Statistic
-                              value={high_24h}
-                              precision={2}
-                              valueStyle={ 
-                                  high_24h >= 0 ? { color: '#3f8600', fontSize: 16 ,marginLeft: 180, marginTop: -25, marginBottom: 50}
-                                   :
-                                   { color: 'red', fontSize: 16 ,marginLeft: 180, marginTop: -25, marginBottom: 50}}
-                              prefix={ high_24h >= 0 ? <ArrowUpOutlined /> : < ArrowDownOutlined />}
-                              suffix="%"
-                            />
-                        </Col>
-                    </Row>
-                    <p>{chart}</p>
-                    <div className="coin_data">
-                        
-                        <p className="price_span"> preço</p>
-                        <p className="coin_price">$ {price.toLocaleString()}</p>
-
-                        <p className="volume_span">volume</p>
-                        <p className="coin_volume">$ {volume.toLocaleString()}</p>
-                    </div>
+            <div className="coin">
+                <img src={image} className="coin_image" alt="crypto" />
+                <h1 className="coin_name">{name}</h1>
+                <p className="coin_symbol">{symbol}</p>
+                <Row>
+                <p className="price_high_24">24hrs</p>
+                    <Col className="high_42">
+                        <Statistic
+                          value={high_24h}
+                          precision={2}
+                          valueStyle={ 
+                              high_24h >= 0 ? { color: '#3f8600', fontSize: 16 ,marginLeft: 180, marginTop: -25, marginBottom: 50}
+                               :
+                               { color: 'red', fontSize: 16 ,marginLeft: 180, marginTop: -25, marginBottom: 50}}
+                          prefix={ high_24h >= 0 ? <ArrowUpOutlined /> : < ArrowDownOutlined />}
+                          suffix="%"
+                        />
+                    </Col>
+                </Row>
+                <p>{chart}</p>
+                <div className="coin_data">
+                    
+                    <p className="price_span"> preço</p>
+                    <p className="coin_price">$ {price.toLocaleString()}</p>
+                    <p className="volume_span">volume</p>
+                    <p className="coin_volume">$ {volume.toLocaleString()}</p>
                 </div>
-            </Link>                    
+            </div>              
         </div>
         </>
     );
