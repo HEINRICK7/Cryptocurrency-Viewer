34fc7f6830d705c9b378847e4125bfcf3be84a83
Henrique <Carlos Henrique>
2021-04-29 08:41:44 -0300

    modified files

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,12 @@ import React from  'react-icons'
 import { Route, BrowserRouter, Switch} from 'react-router-dom'
 
 import Home from './pages/Home';
-import Coins from './pages/CoinsDetailPage'
 
 const Router = () => {
     return(
         <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Home}/>
-            <Route exact path="/coins/:key" component={Coins}/>
         </Switch>
         </BrowserRouter>
     )
